80a0d19fe2fad3b073961f00c5ccf6d3934f40f2
Isaac de Moraes <30375595+Iakim@users.noreply.github.com>
2019-04-03 10:40:32 -0300

    Update ack_zabbix.py

--- ack_zabbix.py
+++ ack_zabbix.py
@@ -9,4 +9,4 @@ password = 'zabbix'
 conexao = ZabbixAPI(server = server)
 conexao.login(username, password)
 
-conexao.event.acknowledge({'eventids': sys.argv[1], 'action': sys.argv[2], 'message': 'Ticket ' + str(sys.argv[3]) + ' criado no OTRS.'})
+conexao.event.acknowledge({'eventids': sys.argv[1], 'action': 2, 'message': 'Ticket ' + str(sys.argv[3]) + ' criado no OTRS.'})
